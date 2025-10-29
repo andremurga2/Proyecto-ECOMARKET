ccf159018af3550f6c21222207faa5d8d4012c5c
andremurga2 <an.murga@duocuc.cl>
2025-10-29 15:22:03 -0300

    actualizacion de rutas, se agrega la ruta para el pago

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,10 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Products from '../pages/Products';
 import Contact from '../pages/Contact';
+import Payments from '../pages/Payments.jsx';
+
 
 function AppRoutes() {
   return (
     <Routes>
+      <Route path="/pagos" element={<Payments />} />
       <Route path="/" element={<Home />} />
       <Route path="/productos" element={<Products />} />
       <Route path="/contacto" element={<Contact />} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { Navbar, Nav, Container, Badge } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { AppContext } from '../context/AppContext';
+import { Link, useNavigate } from 'react-router-dom';
+import { CartContext } from '../context/CartContext.jsx';
 
 function NavBar() {
-  const { cartCount } = useContext(AppContext);
+  const { cartCount } = useContext(CartContext);
+  const navigate = useNavigate(); // Para navegar al detalle de pagos
 
   return (
     <Navbar bg="success" variant="dark" expand="lg" className="mb-4">
@@ -17,7 +18,14 @@ function NavBar() {
             <Nav.Link as={Link} to="/productos">Productos</Nav.Link>
             <Nav.Link as={Link} to="/contacto">Contacto</Nav.Link>
           </Nav>
-          <Badge bg="light" text="dark">🛒 {cartCount}</Badge>
+          <Badge
+            bg="light"
+            text="dark"
+            style={{ cursor: 'pointer' }}
+            onClick={() => navigate('/pagos')} // Aquí navega al detalle del pago
+          >
+            🛒 {cartCount}
+          </Badge>
         </Navbar.Collapse>
       </Container>
     </Navbar>
@@ -25,4 +33,3 @@ function NavBar() {
 }
 
 export default NavBar;
-
